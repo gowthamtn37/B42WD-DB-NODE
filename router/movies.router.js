71d17387185987ee5de7a5f1e8bbeaf56bf0c99c
gowthamtn37 <gowthamtn37@gmail.com>
2023-02-25 09:00:00 +0530

    res

--- router/movies.router.js
+++ router/movies.router.js
@@ -57,7 +57,7 @@ router.delete("/:id", async function (request, response) {
 
 // update
 router.put("/:id", express.json(), async function (request, response) {
-  request.header("Access-Control-Allow-Origin", "*");
+  respond.header("Access-Control-Allow-Origin", "*");
   const { id } = request.params;
   const data = request.body;
   const result = await updateMovies(id, data);
